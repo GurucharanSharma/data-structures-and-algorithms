12cf2671731ae64571392e7bfa3e84bf005fdc31
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-06-03 10:53:40 +0530

    Add: Array DSA questions

--- array/lectures/EquilibriumPoint.java
+++ array/lectures/EquilibriumPoint.java
@@ -1,28 +1,27 @@
 package array.lectures;
 
 public class EquilibriumPoint {
+
     public static void main(String[] args) {
-        int[] array = new int[] { -7, 1, 5, 2, -4, 3, 0 };
+        int[] array = new int[]{1, 2, 3, 4, 5, 2, 9, 4, 6, -4};
         System.out.println(getEquilibriumPoint(array));
     }
 
-    static int getEquilibriumPoint(int[] array) {
+    private static int getEquilibriumPoint(int[] array) {
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+        for (int j : array) {
+            sum += j;
         }
-
-        System.out.println(sum);
 
         int prefixSum = array[0];
         int suffixSum = sum;
         for (int i = 1; i < array.length; i++) {
-            System.out.println("ps = " + prefixSum + " | ss = " + suffixSum);
             prefixSum += array[i];
             suffixSum = suffixSum - array[i - 1];
 
-            if (prefixSum == suffixSum)
+            if (prefixSum == suffixSum) {
                 return i;
+            }
 
         }
 
