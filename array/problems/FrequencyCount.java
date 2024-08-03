c96d5a15e8add42cd01815535baa24cd0c884e37
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-08-04 00:41:51 +0530

    Add: Array DSA questions

---
+++ array/problems/FrequencyCount.java
@@ -0,0 +1,62 @@
+package array.problems;
+
+import java.util.Arrays;
+
+public class FrequencyCount {
+
+  public static void main(String[] args) {
+//    int[] arr = {2, 3, 2, 3, 5};
+//    int p = 5;
+
+//    int[] arr = {3, 3, 3, 3};
+//    int p = 3;
+
+//    int[] arr = {1};
+//    int p = 10;
+
+    int[] arr = {8, 9};
+    int p = 9;
+
+    frequencyCount(arr, arr.length, p);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  /**
+   * Approach 1
+   */
+  //Function to count the frequency of all elements from 1 to N in the array.
+  public static void frequencyCount(int[] arr, int N, int P) {
+    int d = P + 1;
+    for (int i = 0; i < N; i++) {
+      int index = (arr[i] % d) - 1;
+      if (index < N) {
+        arr[index] = (arr[index] + d);
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = arr[i] / d;
+    }
+  }
+
+  /**
+   * Approach 2
+   */
+  //Function to count the frequency of all elements from 1 to N in the array.
+  public static void frequencyCount1(int arr[], int N, int P) {
+    for (int i = 0; i < N; i++) {
+      arr[i] = arr[i] - 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+      int index = arr[i] % (P + 1);
+      if (index < N) {
+        arr[index] = arr[index] + (P + 1);
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = arr[i] / (P + 1);
+    }
+  }
+}
