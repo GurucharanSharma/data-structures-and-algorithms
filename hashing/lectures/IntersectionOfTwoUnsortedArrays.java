82ca110ead8db5dc2d33b5cc10de68cfc065e87e
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-09-26 17:30:25 +0530

    Add: Naive approach implementation

--- hashing/lectures/IntersectionOfTwoUnsortedArrays.java
+++ hashing/lectures/IntersectionOfTwoUnsortedArrays.java
@@ -9,9 +9,27 @@ public class IntersectionOfTwoUnsortedArrays {
   }
 
   /**
-   * Time Complexity: θ(arr.length + brr.length) <br> Auxiliary Space: θ(arr.length / brr.length)
+   * Time Complexity: O(n<sup>2</sup>)
+   * <p>
+   * Auxiliary Space: O(1)
    */
   private static void intersect(int[] arr, int[] brr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < brr.length; j++) {
+        if (arr[i] == brr[j]) {
+          System.out.print(arr[i] + " ");
+          break;
+        }
+      }
+    }
+  }
+
+  /**
+   * Time Complexity: θ(arr.length + brr.length)
+   * <p>
+   * Auxiliary Space: θ(arr.length / brr.length)
+   */
+  private static void intersect1(int[] arr, int[] brr) {
     HashSet<Integer> hashSet = new HashSet<>();
 
     if (arr.length < brr.length) {
@@ -44,5 +62,6 @@ public class IntersectionOfTwoUnsortedArrays {
     int[] brr = {30, 10};
 
     intersect(arr, brr);
+    intersect1(arr, brr);
   }
 }
