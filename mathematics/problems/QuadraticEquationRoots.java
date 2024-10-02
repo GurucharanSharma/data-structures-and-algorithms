f10aeeb0665cf340474b87bec95c0cbe4873fec2
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-02 11:05:49 +0530

    Update QuadraticEquationRoots.java

--- mathematics/problems/QuadraticEquationRoots.java
+++ mathematics/problems/QuadraticEquationRoots.java
@@ -5,17 +5,17 @@ import java.util.Collections;
 
 public class QuadraticEquationRoots {
 
-    public ArrayList<Integer> quadraticRoots(int a, int b, int c) {
-        ArrayList<Integer> roots = new ArrayList<>();
-        int root = (b * b) - (4 * a * c);
-        if (root < 0) {
-            roots.add(-1);
-        } else {
-            roots.add((int) Math.floor(((-1 * b) + Math.sqrt(root)) / (2 * a)));
-            roots.add((int) Math.floor(((-1 * b) - Math.sqrt(root)) / (2 * a)));
-        }
-
-        roots.sort(Collections.reverseOrder());
-        return roots;
+  public ArrayList<Integer> quadraticRoots(int a, int b, int c) {
+    ArrayList<Integer> roots = new ArrayList<>();
+    int discriminant = (b * b) - (4 * a * c);
+    if (discriminant < 0) {
+      roots.add(-1);
+    } else {
+      roots.add((int) Math.floor(((-1 * b) + Math.sqrt(discriminant)) / (2 * a)));
+      roots.add((int) Math.floor(((-1 * b) - Math.sqrt(discriminant)) / (2 * a)));
     }
+
+    roots.sort(Collections.reverseOrder());
+    return roots;
+  }
 }
