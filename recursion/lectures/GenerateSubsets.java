9bbb2f3ae601273f194b09139de8d12a4e398d81
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-02 16:38:55 +0530

    Update GenerateSubsets.java

--- recursion/lectures/GenerateSubsets.java
+++ recursion/lectures/GenerateSubsets.java
@@ -1,17 +1,18 @@
 package recursion.lectures;
 
 public class GenerateSubsets {
-    public static void main(String[] args) {
-        generateSubsets(args[0], "", 0);
+
+  public static void main(String[] args) {
+    generateSubsets(args[0], "", 0);
+  }
+
+  static void generateSubsets(String str, String curr, int i) {
+    if (i == str.length()) {
+      System.out.println(curr);
+      return;
     }
 
-    static void generateSubsets(String str, String curr, int i) {
-        if (i == str.length()) {
-            System.out.println(curr);
-            return;
-        }
-
-        generateSubsets(str, curr, i + 1);
-        generateSubsets(str, curr + str.charAt(i), i + 1);
-    }
+    generateSubsets(str, curr, i + 1);
+    generateSubsets(str, curr + str.charAt(i), i + 1);
+  }
 }
