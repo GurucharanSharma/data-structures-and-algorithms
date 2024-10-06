e7eb72a9c40ff2ee211c0d893777be27dbcae070
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-06 15:26:29 +0530

    Update CheckForRotations.java

--- strings/lectures/CheckForRotations.java
+++ strings/lectures/CheckForRotations.java
@@ -6,18 +6,20 @@ import java.util.List;
 public class CheckForRotations {
 
   public static void main(String[] args) {
-    String input = "ABCD";
-    String pattern = "CDAB";
+//    String input = "ABCD";
+//    String pattern = "CDAB";
 
 //    String input = "ABAAA";
 //    String pattern = "BAAAA";
 
-//    String input = "ABAB";
-//    String pattern = "ABBA";
+    String input = "ABAB";
+    String pattern = "ABBA";
 
     System.out.println(isRotation(input, pattern));
     System.out.println();
     System.out.println(isRotation1(input, pattern));
+    System.out.println();
+    System.out.println(isRotation2(input, pattern));
   }
 
   private static boolean isRotation(String input, String pattern) {
@@ -29,6 +31,10 @@ public class CheckForRotations {
   }
 
   private static boolean isRotation1(String input, String pattern) {
+    if (input.length() != pattern.length()) {
+      return false;
+    }
+
     List<Integer> indexLookup = new ArrayList<>();
     for (int i = 0; i < pattern.length(); i++) {
       if (pattern.charAt(i) == input.charAt(0)) {
@@ -54,5 +60,23 @@ public class CheckForRotations {
     }
 
     return true;
+  }
+
+  private static boolean isRotation2(String input, String pattern) {
+    if (input.length() != pattern.length()) {
+      return false;
+    }
+
+    for (int i = 0; i < input.length(); i++) {
+      if (input.charAt(i) == pattern.charAt(0)) {
+        if (input.substring(i).equals(pattern.substring(0, input.length() - i))) {
+          if (input.substring(0, i).equals(pattern.substring(input.length() - i))) {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
   }
 }
