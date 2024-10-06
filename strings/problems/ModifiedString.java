1e74cb242ad9478463b29f5e65f40cf0c3ae61b7
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-06 09:04:37 +0530

    Update ModifiedString.java

--- strings/problems/ModifiedString.java
+++ strings/problems/ModifiedString.java
@@ -7,8 +7,6 @@ public class ModifiedString {
 //    String S = "aaaaa";
 
     System.out.println(modified(S));
-    System.out.println();
-    System.out.println(modified1(S));
   }
 
   //Function to find minimum number of characters which Ishaan must insert
@@ -27,30 +25,6 @@ public class ModifiedString {
       if (count == 3) {
         result++;
         count = 0;
-      } else {
-        i++;
-      }
-    }
-
-    return result;
-  }
-
-  //Function to find minimum number of characters which Ishaan must insert
-  //such that string doesn't have three consecutive same characters.
-  public static long modified1(String a) {
-    int i = 0;
-    int count = 0;
-    long result = 0;
-    while (i < a.length() - 1) {
-      if (a.charAt(i) == a.charAt(i + 1)) {
-        count++;
-      } else {
-        count = 1;
-      }
-
-      if (count == 3) {
-        result++;
-        count = 1;
       } else {
         i++;
       }
