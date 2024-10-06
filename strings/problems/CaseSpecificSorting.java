8d49a4aad42d56de38d532c789f1fe7c274d9233
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-06 15:06:00 +0530

    Case-specific Sorting of Strings

--- strings/problems/CaseSpecificSorting.java
+++ strings/problems/CaseSpecificSorting.java
@@ -11,9 +11,12 @@ public class CaseSpecificSorting {
     String str = "srbDKi";
 
     System.out.println(caseSort(str));
+    System.out.println();
+    System.out.println(caseSort1(str));
   }
 
-  //Function to perform case-specific sorting of strings.
+  // Function to perform case-specific sorting of strings.
+  // Using List
   public static String caseSort(String str) {
     List<Character> lower = new ArrayList<>();
     List<Character> upper = new ArrayList<>();
@@ -40,6 +43,63 @@ public class CaseSpecificSorting {
         builder.append(lower.get(l++));
       } else if (Character.isUpperCase(ch)) {
         builder.append(upper.get(u++));
+      } else {
+        builder.append(ch);
+      }
+    }
+
+    return builder.toString();
+  }
+
+  // Function to perform case-specific sorting of strings.
+  // Using arrays
+  public static String caseSort1(String str) {
+    int[] lower = new int[26];
+    int[] upper = new int[26];
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (Character.isLowerCase(ch)) {
+        lower[ch - 'a']++;
+      } else if (Character.isUpperCase(ch)) {
+        upper[ch - 'A']++;
+      }
+    }
+
+    int l = 0;
+    int u = 0;
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (Character.isLowerCase(ch)) {
+        if (l < 26 && lower[l] > 0) {
+          builder.append((char) (l + 'a'));
+          lower[l]--;
+        } else {
+          while (l < 26 && lower[l] == 0) {
+            l++;
+          }
+
+          if (l < 26) {
+            builder.append((char) (l + 'a'));
+            lower[l]--;
+          }
+        }
+      } else if (Character.isUpperCase(ch)) {
+        if (u < 26 && upper[u] > 0) {
+          builder.append((char) (u + 'A'));
+          upper[u]--;
+        } else {
+          while (u < 26 && upper[u] == 0) {
+            u++;
+          }
+
+          if (u < 26) {
+            builder.append((char) (u + 'A'));
+            upper[u]--;
+          }
+        }
       } else {
         builder.append(ch);
       }
