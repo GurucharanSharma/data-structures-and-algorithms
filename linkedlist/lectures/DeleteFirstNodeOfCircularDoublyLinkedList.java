fcfa6037adc5a548365a6bc8d0eaf4e12e40326b
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-07 13:10:46 +0530

    Update DeleteFirstNodeOfCircularDoublyLinkedList.java

--- linkedlist/lectures/DeleteFirstNodeOfCircularDoublyLinkedList.java
+++ linkedlist/lectures/DeleteFirstNodeOfCircularDoublyLinkedList.java
@@ -5,7 +5,7 @@ import linkedlist.common.CircularDoublyLinkedList.Node;
 public class DeleteFirstNodeOfCircularDoublyLinkedList {
 
   public static Node execute(Node head) {
-    if (head == null || head.next == null) {
+    if (head == null || head.next == head) {
       return null;
     }
 
