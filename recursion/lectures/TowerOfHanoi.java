a7913cce5684e783186007eb6a36221eb8698d6a
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-09 21:07:45 +0530

    Update TowerOfHanoi.java

--- recursion/lectures/TowerOfHanoi.java
+++ recursion/lectures/TowerOfHanoi.java
@@ -22,6 +22,8 @@ public class TowerOfHanoi {
 
   public static void main(String[] args) {
     moveDisc(2, 1, 2, 3);
+    System.out.println();
+    System.out.println(moveDisc1(2, 1, 3, 2));
   }
 
   static void moveDisc(int disk, int a, int b, int c) {
@@ -33,5 +35,20 @@ public class TowerOfHanoi {
     moveDisc(disk - 1, a, c, b);
     System.out.println("Move disk " + disk + " from " + a + " to " + c);
     moveDisc(disk - 1, b, a, c);
+  }
+
+  // Calculates the total number of moves required to solve the tower of hanoi
+  private static long moveDisc1(int n, int from, int to, int aux) {
+    if (n == 1) {
+      System.out.println("Move disk 1 from rod " + from + " to rod " + to);
+      return 1;
+    }
+
+    long res = 1;
+    res += moveDisc1(n - 1, from, aux, to);
+    System.out.println("Move disk " + n + " from rod " + from + " to rod " + to);
+    res += moveDisc1(n - 1, aux, to, from);
+
+    return res;
   }
 }
