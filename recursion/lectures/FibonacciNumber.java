5db8726c7d8548ec543b21a651c214cd2b01f407
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-09 20:27:26 +0530

    Update FibonacciNumber.java

--- recursion/lectures/FibonacciNumber.java
+++ recursion/lectures/FibonacciNumber.java
@@ -4,46 +4,61 @@ import java.util.Arrays;
 
 public class FibonacciNumber {
 
-    public static void main(String[] args) {
-        System.out.println("Approach 1");
-        System.out.println(generateFibonacci(10));
+  public static void main(String[] args) {
+    System.out.println("Approach 1");
+    System.out.println(generateFibonacci(10));
 
-        System.out.println("Approach 3");
-        int sum = 0;
-        for (int i: generateFibonacci1(10)) {
-            sum = sum + i;
-        }
-        System.out.println(Arrays.toString(generateFibonacci1(10)));
-        System.out.println(sum);
+    System.out.println("Approach 2");
+    System.out.println(generateFibonacci1(10));
+
+    System.out.println("Approach 3");
+    int sum = 0;
+    for (int i : generateFibonacci2(10)) {
+      sum = sum + i;
+    }
+    System.out.println(Arrays.toString(generateFibonacci2(10)));
+    System.out.println(sum);
+  }
+
+  /**
+   * Find nth fibonacci number
+   */
+  static int generateFibonacci(int n) {
+    if (n <= 1) {
+      return n;
     }
 
-    /**
-     * Find nth fibonacci number
-     */
-    static int generateFibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
+    return generateFibonacci(n - 1) + generateFibonacci(n - 2);
+  }
 
-        return generateFibonacci(n - 1) + generateFibonacci(n - 2);
+  static int generateFibonacci1(int n) {
+    return generateSequence(n, 1, 1);
+  }
+
+  private static int generateSequence(int n, int first, int second) {
+    if (n == 1) {
+      return first;
     }
 
-    /**
-     * Generate all n fibonacci numbers
-     */
-    public static int[] generateFibonacci1(int n) {
-        int[] arr = new int[n];
-        fillNumbers(arr, 0, 1, n);
+    return generateSequence(n - 1, second, first + second);
+  }
 
-        return arr;
+  /**
+   * Generate all n fibonacci numbers
+   */
+  public static int[] generateFibonacci2(int n) {
+    int[] arr = new int[n];
+    fillNumbers(arr, 0, 1, n);
+
+    return arr;
+  }
+
+  private static void fillNumbers(int[] arr, int first, int second, int n) {
+    if (n <= 0) {
+      return;
     }
 
-    private static void fillNumbers(int[] arr, int first, int second, int n) {
-        if (n <= 0) {
-            return;
-        }
-
-        arr[arr.length - n] = first;
-        fillNumbers(arr, first + second, first, --n);
-    }
+    arr[arr.length - n] = first;
+    fillNumbers(arr, first + second, first, --n);
+  }
 }
