685d08b24d96b0402d9717263582a7b99accfc2f
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-11 14:03:04 +0530

    Update BalancedParenthesis.java

--- stack/lectures/BalancedParenthesis.java
+++ stack/lectures/BalancedParenthesis.java
@@ -24,6 +24,24 @@ public class BalancedParenthesis {
     return stack.isEmpty();
   }
 
+  public static boolean execute1(String expr) {
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < expr.length(); i++) {
+      if (expr.charAt(i) == '(' || expr.charAt(i) == '{' || expr.charAt(i) == '[') {
+        stack.push(expr.charAt(i));
+      } else {
+        if (stack.isEmpty()) {
+          return false;
+        } else if (!matching(stack.pop(), expr.charAt(i))) {
+          return false;
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+
   private static boolean matching(char a, char b) {
     return (a == '(' && b == ')') || (a == '{' && b == '}') || (a == '[' && b == ']');
   }
