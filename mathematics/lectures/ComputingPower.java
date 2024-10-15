47d596fad6e1b9d2696dec6aebe3a6bba6eae9ec
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-15 13:31:17 +0530

    Update ComputingPower.java

--- mathematics/lectures/ComputingPower.java
+++ mathematics/lectures/ComputingPower.java
@@ -7,8 +7,11 @@ public class ComputingPower {
     int n = 5;
 
     System.out.println(computingPower(x, n));
+    System.out.println();
+    System.out.println(computingPower1(x, n));
   }
 
+  // Recursive approach
   private static int computingPower(int x, int y) {
     if (y == 0) {
       return 1;
@@ -21,5 +24,20 @@ public class ComputingPower {
     } else {
       return x * temp * temp;
     }
+  }
+
+  // Iterative approach
+  private static int computingPower1(int n, int p) {
+    int res = 1;
+    while (p > 0) {
+      if (p % 2 == 1) {
+        res = res * n;
+      }
+
+      n = n * n;
+      p = p / 2;
+    }
+
+    return res;
   }
 }
