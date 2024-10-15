d350c40060fd0903acd8e47c0862bf1de2f63c06
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-15 12:50:21 +0530

    Update MathematicsRunner.java

--- mathematics/MathematicsRunner.java
+++ mathematics/MathematicsRunner.java
@@ -3,17 +3,6 @@ package mathematics;
 public class MathematicsRunner {
 
   public static void main(String[] args) {
-    int n = 10; // 1342177280
 
-    System.out.println(reverseBits(n));
-  }
-
-  private static long reverseBits(int n) {
-    long reverse = 0;
-    for (int i = 0; i < 32; i++) {
-      reverse += (n & (1 << i)) > 0 ? (1 << 31 - i) : 0;
-    }
-
-    return reverse;
   }
 }
