22ddce0d21744556ad40f4562476363fef117367
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-15 15:36:45 +0530

    Update ReverseBits.java

--- mathematics/problems/ReverseBits.java
+++ mathematics/problems/ReverseBits.java
@@ -1,28 +1,33 @@
 package mathematics.problems;
 
-import java.util.Arrays;
-
 public class ReverseBits {
 
   public static void main(String[] args) {
     long n = 10;
 
+    // Takes into account the 32 bit representation of the number
     System.out.println(reverseBits(n));
     System.out.println();
     System.out.println(reverseBits1(n));
     System.out.println();
     System.out.println(reverseBits2(n));
+    System.out.println();
+
+    // Takes the bits only till the last 1 in the binary representation. (variable length)
+    System.out.println(reverseBits3(n));
+    System.out.println();
+    System.out.println(reverseBits4(n));
   }
 
   /**
    * Approach 1
    */
-  public static long reverseBits(long n) {
+  private static long reverseBits(long n) {
     int[] binary = getBinary(n);
-    System.out.println(Arrays.toString(binary));
+//    System.out.println(Arrays.toString(binary));
 
     long reverse = getReverse(binary);
-    System.out.println(Arrays.toString(getBinary(reverse)));
+//    System.out.println(Arrays.toString(getBinary(reverse)));
 
     return reverse;
   }
@@ -57,7 +62,7 @@ public class ReverseBits {
   /**
    * Approach 2
    */
-  public static long reverseBits1(long n) {
+  private static long reverseBits1(long n) {
     int[] bits = new int[32];
 
     // Preparing the bits array.
@@ -95,7 +100,7 @@ public class ReverseBits {
   /**
    * Approach 3
    */
-  public static long reverseBits2(long n) {
+  private static long reverseBits2(long n) {
 
     // 'ans' will store the value of reversed bits representation.
     long ans = 0;
@@ -112,5 +117,52 @@ public class ReverseBits {
     }
 
     return ans;
+  }
+
+  // Approach 4
+  private static long reverseBits3(long n) {
+    long binary = 0;
+    while (n > 0) {
+      if (n % 2 == 1) {
+        binary = (binary * 10) + 1;
+      } else {
+        binary = binary * 10;
+      }
+
+      n = n / 2;
+    }
+
+    int pow = 0;
+    long reverse = 0;
+    while (binary > 0) {
+      if (binary % 10 == 1) {
+        reverse += (int) Math.pow(2, pow);
+      }
+
+      pow++;
+      binary = binary / 10;
+    }
+
+    return reverse;
+  }
+
+  // Approach 5
+  // Using String representation of the binary
+  private static long reverseBits4(long n) {
+    StringBuilder builder = new StringBuilder();
+    while (n > 0) {
+      builder.append(n % 2);
+      n = n / 2;
+    }
+
+    String binary = builder.toString();
+    long reverse = 0;
+    for (int i = 0; i < binary.length(); i++) {
+      if (binary.charAt(i) == '1') {
+        reverse += (int) Math.pow(2, binary.length() - (i + 1));
+      }
+    }
+
+    return reverse;
   }
 }
