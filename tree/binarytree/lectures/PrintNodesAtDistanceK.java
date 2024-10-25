04be2c08dba97e199348a153cb46403bbaf84cf0
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-10-25 18:24:26 +0530

    Update PrintNodesAtDistanceK.java

--- tree/binarytree/lectures/PrintNodesAtDistanceK.java
+++ tree/binarytree/lectures/PrintNodesAtDistanceK.java
@@ -1,9 +1,31 @@
 package tree.binarytree.lectures;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import tree.BinaryTree.Node;
 
 public class PrintNodesAtDistanceK {
 
+  public static void main(String[] args) {
+    Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(8);
+    root.right.left.right = new Node(9);
+    root.left.left.left = new Node(10);
+    root.left.left.right = new Node(11);
+    root.left.left.right.right = new Node(12);
+
+    execute(root, 3);
+    System.out.println();
+    execute1(root, 3);
+  }
+
+  /**
+   * Recursive approach
+   */
   public static void execute(Node root, int k) {
     if (root == null) {
       return;
@@ -15,5 +37,40 @@ public class PrintNodesAtDistanceK {
 
     execute(root.left, k - 1);
     execute(root.right, k - 1);
+  }
+
+  /**
+   * Iterative approach using Queue
+   */
+  public static void execute1(Node root, int k) {
+    if (root == null) {
+      return;
+    }
+
+    Queue<Node> queue = new ArrayDeque<>();
+    queue.add(root);
+
+    int level = 0;
+    while (!queue.isEmpty() && level <= k) {
+      int size = queue.size();
+
+      for (int i = 0; i < size; i++) {
+        Node curr = queue.remove();
+
+        if (level == k) {
+          System.out.print(curr.key + " ");
+        }
+
+        if (curr.left != null) {
+          queue.add(curr.left);
+        }
+
+        if (curr.right != null) {
+          queue.add(curr.right);
+        }
+      }
+
+      level++;
+    }
   }
 }
