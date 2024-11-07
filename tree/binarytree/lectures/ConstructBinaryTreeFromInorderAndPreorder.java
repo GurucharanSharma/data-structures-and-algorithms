1c846dca040e8267fee2c70b1c9001da632ed477
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-07 18:25:41 +0530

    Update ConstructBinaryTreeFromInorderAndPreorder.java

--- tree/binarytree/lectures/ConstructBinaryTreeFromInorderAndPreorder.java
+++ tree/binarytree/lectures/ConstructBinaryTreeFromInorderAndPreorder.java
@@ -1,10 +1,20 @@
 package tree.binarytree.lectures;
 
 import tree.BinaryTree.Node;
+import tree.TreeFormatter;
 
 public class ConstructBinaryTreeFromInorderAndPreorder {
 
+  private static final TreeFormatter formatter = new TreeFormatter();
   private static int preIndex;
+
+  public static void main(String[] args) {
+    int[] in = {20, 10, 40, 30, 50};
+    int[] pre = {10, 20, 30, 40, 50};
+
+    Node root = execute(in, pre, 0, in.length - 1);
+    formatter.topDown(root);
+  }
 
   public static Node execute(int[] in, int[] pre, int is, int ie) {
     if (is > ie) {
