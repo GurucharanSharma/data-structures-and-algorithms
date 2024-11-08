88970ce85b65b887512bafe3d5da3cc67d283540
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-08 16:46:28 +0530

    Update BinaryRepresentation.java

--- recursion/lectures/BinaryRepresentation.java
+++ recursion/lectures/BinaryRepresentation.java
@@ -1,15 +1,17 @@
 package recursion.lectures;
 
 public class BinaryRepresentation {
-    public static void main(String[] args) {
-        convertToBinary(10);
-    }
 
-    static void convertToBinary(int n) {
-        if (n <= 0)
-            return;
+  public static void main(String[] args) {
+    convertToBinary(10);
+  }
 
-        convertToBinary(n / 2);
-        System.out.print(n % 2 + " ");
-    }
+  static void convertToBinary(int n) {
+      if (n <= 0) {
+          return;
+      }
+
+    convertToBinary(n / 2);
+    System.out.print(n % 2 + " ");
+  }
 }
