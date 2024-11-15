51dbb1554d5f959ceafa97a7aaf38aa6e9c73e47
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-15 12:48:59 +0530

    Update LargestElement.java

--- array/lectures/LargestElement.java
+++ array/lectures/LargestElement.java
@@ -1,21 +1,37 @@
 package array.lectures;
 
 public class LargestElement {
-    public static void main(String[] args) {
-        int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        largestElement(array);
+  public static void main(String[] args) {
+    int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    largestElement(array);
+    System.out.println();
+    largestElement1(array);
+  }
+
+  static void largestElement(int[] arr) {
+    int largest = arr[0];
+
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
     }
 
-    static void largestElement(int arr[]) {
-        int largest = arr[0];
+    System.out.println("Largest: " + largest);
+  }
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                largest = arr[i];
-            }
-        }
-
-        System.out.println("Largest: " + largest);
+  private static void largestElement1(int[] arr) {
+    if (arr.length == 0) {
+      System.out.println("Empty array");
     }
+
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      max = Math.max(arr[i], max);
+    }
+
+    System.out.println(max);
+  }
 }
