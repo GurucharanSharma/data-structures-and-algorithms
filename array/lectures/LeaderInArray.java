7f8bb1dfc838f81bc23517edf6b020f1ac6e55ce
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-16 13:45:31 +0530

    Update LeaderInArray.java

--- array/lectures/LeaderInArray.java
+++ array/lectures/LeaderInArray.java
@@ -41,4 +41,21 @@ public class LeaderInArray {
     Collections.reverse(list);
     System.out.println(list);
   }
+
+  // Naive Approach
+  private static void printLeaders2(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      boolean isLeader = true;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] <= arr[j]) {
+          isLeader = false;
+          break;
+        }
+      }
+
+      if (isLeader) {
+        System.out.print(arr[i] + " ");
+      }
+    }
+  }
 }
