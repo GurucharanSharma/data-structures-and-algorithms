0e469cada717b83c1f02096789aba9d596196fe1
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-18 13:04:29 +0530

    Update PrefixSum.java

--- array/lectures/PrefixSum.java
+++ array/lectures/PrefixSum.java
@@ -2,25 +2,46 @@ package array.lectures;
 
 public class PrefixSum {
 
-    public static void main(String[] args) {
-        int[] arr = {2, 8, 3, 9, 6, 5, 4};
-        int[] prefixSum = getPrefixSumArray(arr);
+  public static void main(String[] args) {
+    int[] arr = {2, 8, 3, 9, 6, 5, 4};
+    int start = 1, end = 3;
 
-        System.out.println(getSum(prefixSum, 1, 3));
+    int[] prefixSum = getPrefixSumArray(arr);
+    System.out.println(getSum(prefixSum, start, end));
+
+    int[] prefixSum1 = getPrefixSumArray1(arr);
+    System.out.println(getSum(prefixSum1, start, end));
+  }
+
+  // Efficient Approach
+  private static int[] getPrefixSumArray(int[] arr) {
+    int[] prefixSum = new int[arr.length];
+    prefixSum[0] = arr[0];
+
+    for (int i = 1; i < arr.length; i++) {
+      prefixSum[i] = prefixSum[i - 1] + arr[i];
     }
 
-    private static int[] getPrefixSumArray(int[] arr) {
-        int[] prefixSum = new int[arr.length];
-        prefixSum[0] = arr[0];
+    return prefixSum;
+  }
 
-        for (int i = 1; i < arr.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
-        }
+  // Naive Approach
+  private static int[] getPrefixSumArray1(int[] arr) {
+    int[] prefixSum = new int[arr.length];
+    prefixSum[0] = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      int sum = 0;
+      for (int j = 0; j <= i; j++) {
+        sum += arr[j];
+      }
 
-        return prefixSum;
+      prefixSum[i] = sum;
     }
 
-    private static int getSum(int[] prefixSum, int l, int r) {
-        return prefixSum[r] - prefixSum[l - 1];
-    }
+    return prefixSum;
+  }
+
+  private static int getSum(int[] prefixSum, int l, int r) {
+    return prefixSum[r] - prefixSum[l - 1];
+  }
 }
