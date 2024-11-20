dd7c991a4460fe44f9e303b775581a845f143934
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-20 13:45:15 +0530

    Update UnionOfTwoSortedArrays.java

--- sorting/lectures/UnionOfTwoSortedArrays.java
+++ sorting/lectures/UnionOfTwoSortedArrays.java
@@ -8,6 +8,8 @@ public class UnionOfTwoSortedArrays {
     int[] brr = {2, 20, 20, 20, 40, 40, 40};
 
     union(arr, brr);
+    System.out.println();
+    union1(arr, brr);
   }
 
   private static void union(int[] arr, int[] brr) {
@@ -48,6 +50,48 @@ public class UnionOfTwoSortedArrays {
       if (j > 0 && brr[j] != brr[j - 1]) {
         System.out.print(brr[j] + " ");
       }
+      j++;
+    }
+  }
+
+  private static void union1(int[] arr, int[] brr) {
+    int i = 0;
+    int j = 0;
+    while (i < arr.length && j < brr.length) {
+      while (i < arr.length - 1 && arr[i] == arr[i + 1]) {
+        i++;
+      }
+
+      while (j < brr.length - 1 && brr[j] == brr[j + 1]) {
+        j++;
+      }
+
+      if (arr[i] == brr[j]) {
+        System.out.print(arr[i] + " ");
+        i++;
+        j++;
+      } else if (arr[i] < brr[j]) {
+        System.out.print(arr[i] + " ");
+        i++;
+      } else {
+        System.out.print(brr[j] + " ");
+        j++;
+      }
+    }
+
+    while (i < arr.length) {
+      if (i > 0 && arr[i] != arr[i - 1]) {
+        System.out.print(arr[i] + " ");
+      }
+
+      i++;
+    }
+
+    while (j < brr.length) {
+      if (j > 0 && brr[j] != brr[j - 1]) {
+        System.out.print(brr[j] + " ");
+      }
+
       j++;
     }
   }
