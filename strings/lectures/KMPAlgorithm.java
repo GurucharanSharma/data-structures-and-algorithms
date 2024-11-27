a88110406b1f2001aa79a4c1ec8a1dbb6fb4b1e0
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-27 23:19:57 +0530

    Update KMPAlgorithm.java

--- strings/lectures/KMPAlgorithm.java
+++ strings/lectures/KMPAlgorithm.java
@@ -4,14 +4,17 @@ public class KMPAlgorithm {
 
   public static void main(String[] args) {
     search("ababcababaad", "ababa");
+    search1("ababcababaad", "ababa");
+    System.out.println();
     search("adiabatically", "tica");
+    search1("adiabatically", "tica");
+    System.out.println();
     search("aaaaabaaaaa", "aaaa");
+    search1("aaaaabaaaaa", "aaaa");
   }
 
   /**
-   * Time Complexity: O(n) <br>
-   * Auxiliary Space: O(n) <br>
-   * NOTE: Here n is the length of the text.
+   * Time Complexity: O(n) <br> Auxiliary Space: O(n) <br> NOTE: Here n is the length of the text.
    */
   private static void search(String text, String pattern) {
     int[] lps = new int[pattern.length()];
@@ -29,6 +32,31 @@ public class KMPAlgorithm {
         System.out.println(i - j); // or System.out.println(i - pattern.length());
         j = lps[j - 1]; // because j is already increment by 1 too many, hence decrementing it by 1.
       } else if (i < text.length() && text.charAt(i) != pattern.charAt(j)) {
+        if (j == 0) {
+          i++;
+        } else {
+          j = lps[j - 1];
+        }
+      }
+    }
+  }
+
+  // Approach 2
+  private static void search1(String text, String pattern) {
+    int[] lps = new int[pattern.length()];
+    constructLPS(pattern, lps);
+    int i = 0, j = 0;
+
+    while (i < text.length()) {
+      if (text.charAt(i) == pattern.charAt(j)) {
+        i++;
+        j++;
+
+        if (j == pattern.length()) {
+          System.out.println(i - j); // or System.out.println(i - pattern.length());
+          j = lps[j - 1]; // because j is already increment by 1 too many, hence decrementing it by 1.
+        }
+      } else {
         if (j == 0) {
           i++;
         } else {
