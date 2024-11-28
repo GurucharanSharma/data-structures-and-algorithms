196910273c33fd1cd8afd44bf3442b50b1fb3c0e
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-11-28 23:21:21 +0530

    Update MultiplyTwoMatrices.java

--- matrix/problems/MultiplyTwoMatrices.java
+++ matrix/problems/MultiplyTwoMatrices.java
@@ -49,4 +49,56 @@ public class MultiplyTwoMatrices {
 
     return output;
   }
+
+  // Approach 1
+  static boolean multiplyMatrix(int[][] mat1, int[][] mat2, int[][] result) {
+    if (mat2.length != mat1[0].length) {
+      return false;
+    }
+
+    int[][] res = new int[mat2.length][mat1[0].length];
+
+    for (int i = 0; i < mat1.length; i++) {
+      for (int j = 0; j < mat2[i].length; j++) {
+        for (int k = 0; k < mat1[i].length; k++) {
+          res[i][j] += (mat1[i][k] * mat2[k][j]);
+        }
+      }
+    }
+
+    if (res.length != result.length || res[0].length != result[0].length) {
+      return false;
+    }
+
+    for (int i = 0; i < result.length; i++) {
+      if (!Arrays.equals(res[i], result[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  // Approach 2
+  static boolean multiplyMatrix1(int[][] mat1, int[][] mat2, int[][] result) {
+    if (mat2.length != mat1[0].length) {
+      return false;
+    }
+
+    int[][] res = new int[mat2.length][mat1[0].length];
+
+    for (int i = 0; i < mat1.length; i++) {
+      for (int j = 0; j < mat2[i].length; j++) {
+        for (int k = 0; k < mat1[i].length; k++) {
+          res[i][j] += (mat1[i][k] * mat2[k][j]);
+        }
+
+        if (res[i][j] != result[i][j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 }
