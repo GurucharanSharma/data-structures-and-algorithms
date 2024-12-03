955af69168cb3c4227eecdbd47b6a494f7715573
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-12-03 23:39:59 +0530

    Update IncreasingTripletSubsequence.java

--- array/problems/IncreasingTripletSubsequence.java
+++ array/problems/IncreasingTripletSubsequence.java
@@ -34,7 +34,6 @@ public class IncreasingTripletSubsequence {
     public boolean increasingTriplet(int[] nums) {
       int first = Integer.MAX_VALUE;
       int second = Integer.MAX_VALUE;
-      int third = Integer.MAX_VALUE;
 
       for (int i = 0; i < nums.length; i++) {
         if (nums[i] <= first) {
