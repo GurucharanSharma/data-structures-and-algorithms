b15563904b835f0e93fe5fb5f6b7fdb571817441
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-12-04 21:19:45 +0530

    Update UnionOfTwoSortedArrays.java

--- sorting/lectures/UnionOfTwoSortedArrays.java
+++ sorting/lectures/UnionOfTwoSortedArrays.java
@@ -4,8 +4,11 @@ public class UnionOfTwoSortedArrays {
 
   public static void main(String[] args) {
 
-    int[] arr = {10, 20, 20, 40, 60, 60, 60, 60};
-    int[] brr = {2, 20, 20, 20, 40, 40, 40};
+//    int[] arr = {10, 20, 20, 40, 60, 60, 60, 60};
+//    int[] brr = {2, 20, 20, 20, 40, 40, 40};
+
+    int[] arr = {1, 1, 1, 1, 1};
+    int[] brr = {2, 2, 2, 2, 2};
 
     union(arr, brr);
     System.out.println();
@@ -40,14 +43,14 @@ public class UnionOfTwoSortedArrays {
     }
 
     while (i < arr.length) {
-      if (i > 0 && arr[i] != arr[i - 1]) {
+      if (i == 0 || (i > 0 && arr[i] != arr[i - 1])) {
         System.out.print(arr[i] + " ");
       }
       i++;
     }
 
     while (j < brr.length) {
-      if (j > 0 && brr[j] != brr[j - 1]) {
+      if (j == 0 || (j > 0 && brr[j] != brr[j - 1])) {
         System.out.print(brr[j] + " ");
       }
       j++;
@@ -57,13 +60,24 @@ public class UnionOfTwoSortedArrays {
   private static void union1(int[] arr, int[] brr) {
     int i = 0;
     int j = 0;
+
     while (i < arr.length && j < brr.length) {
-      while (i < arr.length - 1 && arr[i] == arr[i + 1]) {
+      while (i > 0 && i < arr.length && arr[i] == arr[i - 1]) {
         i++;
       }
 
-      while (j < brr.length - 1 && brr[j] == brr[j + 1]) {
+      while (j > 0 && j < brr.length && brr[j] == brr[j - 1]) {
         j++;
+      }
+
+      if (i == arr.length && j == 0) {
+        System.out.print(brr[j] + " ");
+        break;
+      } else if (j == brr.length && i == 0) {
+        System.out.print(arr[i] + " ");
+        break;
+      } else if (i == arr.length && j == brr.length) {
+        break;
       }
 
       if (arr[i] == brr[j]) {
