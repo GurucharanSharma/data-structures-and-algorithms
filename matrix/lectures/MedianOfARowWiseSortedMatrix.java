5dc61810d47cbaf057da485710fcfc331ff352ef
Gurucharan Sharma <guru.thecharan@gmail.com>
2024-12-11 12:19:43 +0530

    Update MedianOfARowWiseSortedMatrix.java

--- matrix/lectures/MedianOfARowWiseSortedMatrix.java
+++ matrix/lectures/MedianOfARowWiseSortedMatrix.java
@@ -5,18 +5,36 @@ import java.util.Arrays;
 public class MedianOfARowWiseSortedMatrix {
 
   public static void main(String[] args) {
-    int[][] arr = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10},
-        {11, 12, 13, 14, 15},
-        {16, 17, 18, 19, 20},
-        {21, 22, 23, 24, 25}
-    };
+//    int[][] arr = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+
+//    int[][] arr = {{5, 10, 20, 30, 40}, {1, 2, 3, 4, 6}, {11, 13, 15, 17, 19}};
+
+    int[][] arr = {{1, 3, 4}, {2, 5, 6}, {7, 8, 9}};
 
     System.out.println(median(arr));
+    System.out.println();
+    System.out.println(median1(arr));
   }
 
+  // Naive Approach
   private static int median(int[][] matrix) {
+    int[] arr = new int[matrix.length * matrix[0].length];
+
+    int index = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        arr[index++] = matrix[i][j];
+      }
+    }
+
+    Arrays.sort(arr);
+
+    index = (matrix.length * matrix[0].length + 1) / 2;
+    return arr[index - 1];
+  }
+
+  // Efficient Approach
+  private static int median1(int[][] matrix) {
     int min = matrix[0][0];
     int max = matrix[0][matrix[0].length - 1];
 
