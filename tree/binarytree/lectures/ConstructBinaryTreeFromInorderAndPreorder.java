82b0831df0459926ebfc6a84067b371e17f4d84c
Gurucharan Sharma <guru.thecharan@gmail.com>
2025-01-22 11:14:30 +0530

    Update ConstructBinaryTreeFromInorderAndPreorder.java

--- tree/binarytree/lectures/ConstructBinaryTreeFromInorderAndPreorder.java
+++ tree/binarytree/lectures/ConstructBinaryTreeFromInorderAndPreorder.java
@@ -12,8 +12,11 @@ public class ConstructBinaryTreeFromInorderAndPreorder {
     int[] in = {20, 10, 40, 30, 50};
     int[] pre = {10, 20, 30, 40, 50};
 
-    Node root = execute(in, pre, 0, in.length - 1);
-    formatter.topDown(root);
+    Node root1 = execute(in, pre, 0, in.length - 1);
+    formatter.topDown(root1);
+
+    Node root2 = execute1(in, pre, 0, pre.length - 1, 0, in.length - 1);
+    formatter.topDown(root2);
   }
 
   public static Node execute(int[] in, int[] pre, int is, int ie) {
@@ -26,6 +29,21 @@ public class ConstructBinaryTreeFromInorderAndPreorder {
 
     node.left = execute(in, pre, is, inIndex - 1);
     node.right = execute(in, pre, inIndex + 1, ie);
+    return node;
+  }
+
+  public static Node execute1(int[] in, int[] pre, int ps, int pe, int is, int ie) {
+    if (is > ie || ps > pe) {
+      return null;
+    }
+
+    Node node = new Node(pre[ps]);
+    int inIndex = getInIndex(in, node.key);
+    int nodesOnLeft = (inIndex - is);
+
+    node.left = execute1(in, pre, ps + 1, ps + nodesOnLeft, is, inIndex - 1);
+    node.right = execute1(in, pre, ps + nodesOnLeft + 1, pe, inIndex + 1, ie);
+
     return node;
   }
 
