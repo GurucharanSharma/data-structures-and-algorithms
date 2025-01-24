0a9b0428d08a6bfb27d3106133548fef396e6cef
Gurucharan Sharma <guru.thecharan@gmail.com>
2025-01-24 10:17:04 +0530

    Update LowestCommonAncestor.java

--- tree/binarytree/lectures/LowestCommonAncestor.java
+++ tree/binarytree/lectures/LowestCommonAncestor.java
@@ -1,9 +1,42 @@
 package tree.binarytree.lectures;
 
 import java.util.ArrayList;
+import tree.BinaryTree;
 import tree.BinaryTree.Node;
+import tree.TreeFormatter;
 
 public class LowestCommonAncestor {
+
+  public static void main(String[] args) {
+    BinaryTree tree = new BinaryTree();
+    tree.root = new Node(1);
+    tree.root.left = new Node(2);
+    tree.root.right = new Node(3);
+    tree.root.left.left = new Node(4);
+    tree.root.left.right = new Node(5);
+    tree.root.right.left = new Node(8);
+    tree.root.right.left.right = new Node(9);
+    tree.root.left.left.left = new Node(10);
+    tree.root.left.left.right = new Node(11);
+    tree.root.left.left.right.right = new Node(12);
+
+    TreeFormatter formatter = new TreeFormatter();
+    formatter.topDown(tree.root);
+
+    System.out.println("\n# Lowest Common Ancestor => ");
+    System.out.println("Naive Approach: ");
+    LowestCommonAncestor.execute(tree.root, 10, 12);
+//    LowestCommonAncestor.execute(tree.root, 5, 9);
+//    LowestCommonAncestor.execute(tree.root, 8, 9);
+
+//    System.out.println("Efficient Approach: ");
+//    Node lca = LowestCommonAncestor.execute1(tree.root, 10, 12);
+//    System.out.println(lca == null ? -1 : lca.key);
+//    lca = LowestCommonAncestor.execute1(tree.root, 5, 9);
+//    System.out.println(lca == null ? -1 : lca.key);
+//    lca = LowestCommonAncestor.execute1(tree.root, 8, 9);
+//    System.out.println(lca == null ? -1 : lca.key);
+  }
 
   /**
    * Time Complexity: O(N) where N is the number of nodes in the given Tree. It required 3 traversals.<br> Auxiliary Space: O(N)
@@ -19,6 +52,11 @@ public class LowestCommonAncestor {
     if (!findPath(root, path1, n1) || !findPath(root, path2, n2)) {
       return;
     }
+
+    System.out.println("Paths:");
+    System.out.println(path1);
+    System.out.println(path2);
+    System.out.println();
 
     int i = 0;
     for (; i < path1.size() - 1 && i < path2.size() - 1; i++) {
@@ -49,8 +87,7 @@ public class LowestCommonAncestor {
       return;
     }
 
-    int i = 0;
-    for (; i < path1.size() && i < path2.size(); i++) {
+    for (int i = 0; i < path1.size() && i < path2.size(); i++) {
       if (path1.get(i) != path2.get(i)) {
         if (i > 0) {
           System.out.println(path1.get(i - 1).key);
