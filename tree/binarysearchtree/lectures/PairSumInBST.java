2e8534963403b361f9606bf5e8a88d9d8050ec8c
Gurucharan Sharma <guru.thecharan@gmail.com>
2025-01-30 09:54:44 +0530

    Update PairSumInBST.java

--- tree/binarysearchtree/lectures/PairSumInBST.java
+++ tree/binarysearchtree/lectures/PairSumInBST.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Stack;
+import tree.BinaryTree;
 import tree.BinaryTree.Node;
+import tree.TreeFormatter;
 
 public class PairSumInBST {
 
+  private static final TreeFormatter FORMATTER = new TreeFormatter();
   private static final HashSet<Integer> hashSet = new HashSet<>();
+
+  public static void main(String[] args) {
+    BinaryTree tree = new BinaryTree();
+    tree.root = new Node(15);
+    tree.root.left = new Node(5);
+    tree.root.left.left = new Node(3);
+    tree.root.right = new Node(20);
+    tree.root.right.left = new Node(18);
+    tree.root.right.left.left = new Node(16);
+    tree.root.right.right = new Node(80);
+
+    FORMATTER.topDown(tree.root);
+
+    System.out.println(execute(tree.root, 20));
+    System.out.println(execute(tree.root, 38));
+    System.out.println(execute(tree.root, 30));
+    System.out.println(execute(tree.root, 165));
+    System.out.println(execute(tree.root, 170));
+  }
 
   // Naive Approach: Using in-order traversal and two pointer approach
   // Time Complexity: O(n)
