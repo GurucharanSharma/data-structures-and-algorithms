a1241c82a1ec19e64172efacfac6ac8ed9916463
Gurucharan Sharma <guru.thecharan@gmail.com>
2025-04-29 23:09:01 +0530

    Refactor DisjointSet to implement union by size and update main method for testing

--- graph/problems/DisjointSet.java
+++ graph/problems/DisjointSet.java
@@ -5,36 +5,45 @@ import java.util.Map;
 
 public class DisjointSet {
 
-    public static void main(String[] args) {
-        DisjointSet disjointSet = new DisjointSet(7);
-        disjointSet.unionByRank(1, 2);
-        disjointSet.unionByRank(2, 3);
-        disjointSet.unionByRank(4, 5);
-        disjointSet.unionByRank(6, 7);
-        disjointSet.unionByRank(5, 6);
-
-        if (disjointSet.findUltimateParent(3) == disjointSet.findUltimateParent(7)) {
-            System.out.println("Same");
-        } else {
-            System.out.println("Not Same");
-        }
-
-        disjointSet.unionByRank(3, 7);
-
-        if (disjointSet.findUltimateParent(3) == disjointSet.findUltimateParent(7)) {
-            System.out.println("Same");
-        } else {
-            System.out.println("Not Same");
-        }
-    }
-
     Map<Integer, Integer> ranks = new HashMap<>();
     Map<Integer, Integer> parents = new HashMap<>();
+    Map<Integer, Integer> sizes = new HashMap<>();
 
     DisjointSet(int n) {
         for (int i = 0; i <= n; i++) {
             ranks.put(i, 0);
             parents.put(i, i);
+            sizes.put(i, 1);
+        }
+    }
+
+    public static void main(String[] args) {
+//        DisjointSet disjointSet = new DisjointSet(7);
+//        disjointSet.unionByRank(1, 2);
+//        disjointSet.unionByRank(2, 3);
+//        disjointSet.unionByRank(4, 5);
+//        disjointSet.unionByRank(6, 7);
+//        disjointSet.unionByRank(5, 6);
+
+        DisjointSet disjointSet = new DisjointSet(7);
+        disjointSet.unionBySize(1, 2);
+        disjointSet.unionBySize(2, 3);
+        disjointSet.unionBySize(4, 5);
+        disjointSet.unionBySize(6, 7);
+        disjointSet.unionBySize(5, 6);
+
+        if (disjointSet.findUltimateParent(3) == disjointSet.findUltimateParent(7)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not Same");
+        }
+
+        disjointSet.unionBySize(3, 7);
+
+        if (disjointSet.findUltimateParent(3) == disjointSet.findUltimateParent(7)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not Same");
         }
     }
 
@@ -64,6 +73,23 @@ public class DisjointSet {
         } else {
             parents.put(uParentV, uParentU);
             ranks.put(uParentU, ranks.get(uParentU) + 1);
+        }
+    }
+
+    private void unionBySize(int u, int v) {
+        int uParentU = findUltimateParent(u);
+        int uParentV = findUltimateParent(v);
+
+        if (uParentU == uParentV) {
+            return;
+        }
+
+        if (sizes.get(uParentU) < sizes.get(uParentV)) {
+            parents.put(uParentU, uParentV);
+            sizes.put(uParentV, sizes.get(uParentU) + sizes.get(uParentV));
+        } else {
+            parents.put(uParentV, uParentU);
+            sizes.put(uParentU, sizes.get(uParentU) + sizes.get(uParentV));
         }
     }
 }
