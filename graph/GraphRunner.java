4438657d87978a0803841c8e3ced645f0a15f949
Gurucharan Sharma <guru.thecharan@gmail.com>
2025-04-29 23:08:42 +0530

    Refactor DisjointSet implementation to use union by size and clean up GraphRunner

--- graph/GraphRunner.java
+++ graph/GraphRunner.java
@@ -1,17 +1,8 @@
 package graph;
 
-import graph.common.Graph.AdjacencyList;
-
 public class GraphRunner {
 
     public static void main(String[] args) {
-        AdjacencyList adjacencyList = new AdjacencyList(5);
 
-        adjacencyList.addEdge(1, 2);
-        adjacencyList.addEdge(1, 3);
-        adjacencyList.addEdge(3, 2);
-        adjacencyList.addEdge(4, 5);
-
-        adjacencyList.printGraph();
     }
 }
